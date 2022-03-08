36a712cc714fd985c317f1a1c5fbeb01a2c935e6
Vishnuram <36101678+vishnuram1999@users.noreply.github.com>
2022-03-08 18:17:49 -0500

    Add files via upload

---
+++ flames.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+def flames(name1, name2):
+    total = len(name1) + len(name2)
+    common_letters = Counter(name1) & Counter(name2)
+    count = sum(common_letters.values())*2
+    offset1 = total - count
+
+    flames_list = ['Friend', 'Love', 'Affection', 'Marriage', 'Enemy', 'Siblings']
+
+    for i in range(0, 5):
+        if offset1 > len(flames_list):
+            offset = offset1 % len(flames_list)
+        if offset1 > len(flames_list) and offset%len(flames_list) == 0:
+            offset = len(flames_list)
+        list1 = flames_list[(offset-1)+1:]
+        list2 = flames_list[:(offset-1)]
+        list1.extend(list2)
+        flames_list = list1
+
+    return flames_list[0]
+        
+if __name__ == '__main__':
+    print("***********************Welcome to the Flames Calculator!***********************")
+    name1 = input("Enter name 1: ")
+    name2 = input("Enter name 2: ")
+    print("Result: ", flames(name1, name2))
+    print("***********************Thank you for using the Flames Calculator!***********************")
